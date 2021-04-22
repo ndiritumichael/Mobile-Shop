3b9bd535e7199cb526eac7f3bf55cbf35ab1bde4
ndiritumichael <ndiritumichael5@gmail.com>
2021-04-22 23:02:47 +0300

    Added a cart activity to view saved items

--- app/src/main/java/com/devmike/mobilegrocery/data/ProductsDao.kt
+++ app/src/main/java/com/devmike/mobilegrocery/data/ProductsDao.kt
@@ -1,9 +1,8 @@
 package com.devmike.mobilegrocery.data
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.devmike.mobilegrocery.models.OrdersItem
 import com.devmike.mobilegrocery.models.Product
 import kotlinx.coroutines.flow.Flow
 
@@ -15,5 +14,24 @@ suspend fun insertProducts(product: Product)
 
 @Query("SELECT * FROM Product ")
 fun getAllProducts():Flow<List<Product>>
+
+@Insert(onConflict = OnConflictStrategy.REPLACE)
+suspend fun insertOrder(orderItem:OrdersItem)
+
+@Update
+suspend fun updateOrder(orderItem: OrdersItem)
+
+@Query("SELECT * FROM OrdersItem")
+fun getAllOrders():Flow<List<OrdersItem>>
+
+@Query("SELECT * FROM OrdersItem where productId = :id " )
+suspend fun getSingleOrder(id:Int):List<OrdersItem>
+
+
+@Query("DELETE  FROM OrdersItem ")
+suspend fun clearCart()
+
+@Query("DELETE FROM OrdersItem WHERE orderItemId = :id")
+suspend fun deleteSingleOrder(id: Int)
 
 }
--- app/src/main/java/com/devmike/mobilegrocery/ui/productDetails/ProductsDetailsViewModel.kt
+++ app/src/main/java/com/devmike/mobilegrocery/ui/productDetails/ProductsDetailsViewModel.kt
@@ -1,15 +1,17 @@
 package com.devmike.mobilegrocery.ui.productDetails
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.devmike.mobilegrocery.models.Product
 import com.devmike.mobilegrocery.repository.Repository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ProductsDetailsViewModel @Inject constructor( val repository: Repository) : ViewModel() {
 
-    fun addCart(product: Product){
+    fun addCart(product: Product) = viewModelScope.launch{
         repository.addOrderitem(product)
     }
 
